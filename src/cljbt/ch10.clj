2dab638a9cd5ba19858756d09353fab5d24bbfea
Chris Wolfe <chriswwolfe@gmail.com>
2016-04-26 18:26:37 -0500

    use a state atom

--- src/cljbt/ch10.clj
+++ src/cljbt/ch10.clj
@@ -54,3 +54,29 @@
    (let [potion-val (:potion (:bag healer))]
      (alter target update :hp + potion-val)
      (alter healer update :bag dissoc :potion))))
+
+;; (def state (atom {:bob {:hp 15
+;;                         :bag {}}
+;;                   :jan {:hp 40
+;;                         :bag {:potion 40}}}))
+
+
+;; I prefer the following to using refs
+;; This has a single state atom
+;; To heal, there is a function that will be used to update the game state
+;; the function can be tested in isolation, since it is a pure function.
+;; The state transformer only invokes the function; this way a single
+;; transformation is responsible for healing a player and removing the item.
+(defn better-heal
+  "heal a player"
+  [state target healer]
+  (let [target-data (target state)
+        healer-data (healer state)
+        potion-value (:potion (:bag healer-data))
+        ;; could probably just use update
+        s1 (update-in state [target :hp] + potion-value)]
+    (update-in s1 [healer :bag] dissoc :potion)))
+
+(defn heal-state
+  [state target healer]
+  (swap! state better-heal target healer))
--- test/cljbt/ch10_test.clj
+++ test/cljbt/ch10_test.clj
@@ -13,26 +13,34 @@
   (testing "counts"
     (is (= {"a" 1 "b" 1} (ch10/count-words "a b \n c")))))
 
-(def jan
-  (ref {:hp 15
-        :max-hp 30
-        :bag []}))
+;; (deftest heal!-tests
+;;   (testing "heals"
+;;     (let [target (ref {:hp 15
+;;                        :max-hp 30
+;;                        :bag {}})
+;;           healer (ref {:hp 40
+;;                        :max-hp 40
+;;                        :bag {:potion 30}})]
+;;       (is (= 15 (:hp @target)))
+;;       (ch10/heal! target healer)
+;;       (is (= 45 (:hp @target)))
+;;       (is (= {} (:bag @target))))))
 
-(def bob
-  (ref {:hp 40
-        :max-hp 40
-        :bag {:potion 30}}))
-
-
-(deftest heal!-tests
+(deftest better-heal-tests
   (testing "heals"
-    (let [target (ref {:hp 15
-                       :max-hp 30
-                       :bag {}})
-          healer (ref {:hp 40
-                       :max-hp 40
-                       :bag {:potion 30}})]
-      (is (= 15 (:hp @target)))
-      (ch10/heal! target healer)
-      (is (= 45 (:hp @target)))
-      (is (= {} (:bag @target))))))
+    (let [state {:jan {:hp 15
+                       :bag {}}
+                 :bob {:hp 40
+                       :bag {:potion 30}}}
+          after (ch10/better-heal state :jan :bob)]
+      (is (= 45 (:hp (:jan after))))
+      (is (= {} (:bag (:bob after)))))))
+
+(deftest heal-state
+  (let [state (atom {:jan {:hp 15
+                           :bag {}}
+                     :bob {:hp 40
+                           :bag {:potion 30}}})]
+    (ch10/heal-state state :jan :bob)
+    (is (= 45 (:hp (:jan @state))))
+    (is (= {} (:bag (:bob @state))))))
