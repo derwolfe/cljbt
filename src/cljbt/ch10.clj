64a4273f765d9775363478a577ee9330191829f4
Chris Wolfe <chriswwolfe@gmail.com>
2016-04-26 15:11:00 -0500

    end state -> should just use atom, no need for ref

--- src/cljbt/ch10.clj
+++ src/cljbt/ch10.clj
@@ -5,17 +5,18 @@
             [clojure.string :as str]
             [aleph.http :as http]))
 
-
 ;; this is a the atom containing all of the state
-(def quotes (atom []))
+(def quotes (atom {}))
 (def quote-addr "http://www.braveclojure.com/random-quote")
 
 (defn count-words
   [words]
-  (->> (str/split words #"--" )
+  (->> (str/split-lines words)
        first
-       #(str/split % #" ")
-       #(frequencies %)))
+       str/lower-case
+       ;; eww why invoke self.
+       (#(str/split % #"\s+"))
+       frequencies))
 
 (defn fetch!
   []
@@ -23,7 +24,13 @@
    (http/get quote-addr)
    :body
    bs/to-string
-   (fn [quote] (swap! quotes conj quote))))
+   (fn [quote]
+     (swap!
+      quotes
+      (fn [old]
+        (merge-with +
+                    old
+                    (count-words quote)))))))
 
 ;; could make a stream, realize-each, read-from stream too
 (defn fetch-n-quotes
@@ -34,3 +41,16 @@
     (for [_ (range n)]
       (fetch!)))
    (fn [_] @quotes)))
+
+
+;; lets say that jan heals bob
+(defn heal!
+  "Requires that the healer have a potion; this potion will be used as much as
+  possible to get the character back to max-health."
+  [target healer]
+  (dosync
+   ;; update the healer, remove the potion as it will be entirely used up
+   ;; XXX this doesn't work. (:potion (:bag healer)) seems to be returning nil
+   (let [potion-val (:potion (:bag healer))]
+     (alter target update :hp + potion-val)
+     (alter healer update :bag dissoc :potion))))
--- test/cljbt/ch10_test.clj
+++ test/cljbt/ch10_test.clj
@@ -7,8 +7,32 @@
 
 (deftest fetch-n-quotes-tests
   (testing "fetches some stuff"
-    (is (= 5 (count @(ch10/fetch-n-quotes 5))))))
+    (is (< 0 (count @(ch10/fetch-n-quotes 5))))))
 
 (deftest count-words
   (testing "counts"
-    (is (= {"a" 1 "b" 2} (ch10/count-words ["a b b"])))))
+    (is (= {"a" 1 "b" 1} (ch10/count-words "a b \n c")))))
+
+(def jan
+  (ref {:hp 15
+        :max-hp 30
+        :bag []}))
+
+(def bob
+  (ref {:hp 40
+        :max-hp 40
+        :bag {:potion 30}}))
+
+
+(deftest heal!-tests
+  (testing "heals"
+    (let [target (ref {:hp 15
+                       :max-hp 30
+                       :bag {}})
+          healer (ref {:hp 40
+                       :max-hp 40
+                       :bag {:potion 30}})]
+      (is (= 15 (:hp @target)))
+      (ch10/heal! target healer)
+      (is (= 45 (:hp @target)))
+      (is (= {} (:bag @target))))))
